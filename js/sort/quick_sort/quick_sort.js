b4330a716205f77c8f00227376e3a321fabab6a5
clownegg <shun-0405-uver@outlook.jp>
2025-02-13 03:15:43 +0900

    #

--- js/sort/quick_sort/quick_sort.js
+++ js/sort/quick_sort/quick_sort.js
@@ -1,27 +1,20 @@
 /**
- * @param {Array<number>} arr 
+ * @param {Array<number>} arr
  */
 export const quickSort = (arr) => {
-  if (arr.length <= 1) return arr
+	if (arr.length <= 1) return arr;
 
-  const middle = Math.floor(arr.length / 2);
+	const middle = Math.floor(arr.length / 2);
 
-  const left = [];
-  const right = [];
+	const left = [];
+	const right = [];
 
-  for (let i = 0; i < arr.length; i ++) {
-    if (i === middle) continue
+	for (let i = 0; i < arr.length; i++) {
+		if (i === middle) continue;
 
-    if (arr[i] < arr[middle]) {
-      left.push(arr[i])
-    } else {
-      right.push(arr[i])
-    }
-  }
+		if (arr[i] < arr[middle]) left.push(arr[i]);
+		else right.push(arr[i]);
+	}
 
-  return [
-    ...quickSort(left),
-    arr[middle],
-    ...quickSort(right)
-  ]
-}
+	return [...quickSort(left), arr[middle], ...quickSort(right)];
+};
